bfe30f208ce89c52db3517558a47244457d73be3
Alex <77624572+Kaiman30@users.noreply.github.com>
2021-03-30 15:59:02 +0500

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -2,7 +2,7 @@ import pyautogui as pag
 import keyboard as key
 from time import sleep
 
-end_button = input('Введите кнопку выключения (только латинские буквы): ')
+end_button = input('Enter the shutdown button (only Latin letters): ')
 
 sleep(3.5)
 
